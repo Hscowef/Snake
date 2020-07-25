5d6e84b7206e0459d33d8877e579498ba895fda1
Le_Charooo <Raphaelmoralibigras@outook.fr>
2020-07-25 00:14:53 -0400

    Added pause during game

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-extern crate minifb;
-
 use std::time::SystemTime;
 
 use minifb::{Key, Window, WindowOptions};
@@ -11,12 +9,9 @@ const GRID_DIMENSIONS: usize = 25;
 const UPDATE_TIME: u128 = 200;
 
 const FRAME_DIMENSION: usize = SQUARE_SIZE * GRID_DIMENSIONS;
-
 fn main() {
     let mut game = SnakeGame::new();
     game.play()
-
-    
 }
 
 #[derive(PartialEq)]
@@ -25,7 +20,7 @@ enum Direction {
     Down,
     Left,
     Right,
-    None
+    None,
 }
 
 struct SnakeGame {
@@ -34,7 +29,7 @@ struct SnakeGame {
     food: usize,
     dead: bool,
     windows_buffer: Vec<u32>,
-    move_clock: SystemTime
+    move_clock: SystemTime,
 }
 
 impl SnakeGame {
@@ -45,7 +40,7 @@ impl SnakeGame {
             food: rand::thread_rng().gen_range(0, GRID_DIMENSIONS * GRID_DIMENSIONS),
             dead: false,
             windows_buffer: vec![(255 << 16) | (255 << 8) | 255; FRAME_DIMENSION * FRAME_DIMENSION],
-            move_clock: SystemTime::now()
+            move_clock: SystemTime::now(),
         }
     }
 
@@ -56,31 +51,27 @@ impl SnakeGame {
             FRAME_DIMENSION,
             WindowOptions::default(),
         )
-        .unwrap_or_else(|e| {
-            panic!("{}", e);
-        });
-            
+        .unwrap();
+
         while window.is_open() && !window.is_key_down(Key::Escape) {
             if self.dead {
                 println!("You lost, press enter to play again.");
                 loop {
                     if window.is_key_down(Key::Enter) {
                         self.reset();
-                        break
+                        break;
                     }
 
                     window
                         .update_with_buffer(&self.windows_buffer, FRAME_DIMENSION, FRAME_DIMENSION)
                         .unwrap();
                 }
-
             }
 
             if window.is_key_down(Key::Up) {
                 if self.snake_direction != Direction::Down {
                     self.snake_direction = Direction::Up
                 }
-
             } else if window.is_key_down(Key::Down) {
                 if self.snake_direction != Direction::Up {
                     self.snake_direction = Direction::Down
@@ -93,8 +84,9 @@ impl SnakeGame {
                 if self.snake_direction != Direction::Left {
                     self.snake_direction = Direction::Right
                 }
+            } else if window.is_key_down(Key::Enter) {
+                self.snake_direction = Direction::None;
             }
-            
             if self.move_clock.elapsed().unwrap().as_millis() >= UPDATE_TIME {
                 self.move_snake();
                 self.move_clock = SystemTime::now()
@@ -109,7 +101,6 @@ impl SnakeGame {
 
     fn reset(&mut self) {
         let mut _new_game = Self::new();
-        
         self.snake_position = _new_game.snake_position;
         self.snake_direction = _new_game.snake_direction;
         self.food = _new_game.food;
@@ -122,7 +113,7 @@ impl SnakeGame {
         let mut buffer: Vec<u32> = vec![];
 
         for rows in 0..GRID_DIMENSIONS {
-            let mut r = vec![]; 
+            let mut r = vec![];
 
             for val in 0..GRID_DIMENSIONS {
                 for _ in 0..SQUARE_SIZE {
@@ -135,7 +126,6 @@ impl SnakeGame {
                         r.push((255 << 16) | (255 << 8) | 255)
                     }
                 }
-
             }
 
             for _ in 0..SQUARE_SIZE {
@@ -158,37 +148,38 @@ impl SnakeGame {
                 self.food = random_nbr;
                 return;
             }
-        }   
+        }
     }
 
     fn move_snake(&mut self) {
-
         match self.snake_direction {
             Direction::Up => {
                 let in_first_row = self.snake_position[0] < GRID_DIMENSIONS;
                 if in_first_row {
                     self.dead = true;
-                    return
+                    return;
                 }
-                
-                self.snake_position.insert(0, self.snake_position[0] - GRID_DIMENSIONS);
-            },
+
+                self.snake_position
+                    .insert(0, self.snake_position[0] - GRID_DIMENSIONS);
+            }
             Direction::Down => {
-                let in_last_row = self.snake_position[0] >= (GRID_DIMENSIONS * GRID_DIMENSIONS) - GRID_DIMENSIONS;
+                let in_last_row =
+                    self.snake_position[0] >= (GRID_DIMENSIONS * GRID_DIMENSIONS) - GRID_DIMENSIONS;
                 if in_last_row {
                     self.dead = true;
-                    return
+                    return;
                 }
-                
-                self.snake_position.insert(0, self.snake_position[0] + GRID_DIMENSIONS);
+
+                self.snake_position
+                    .insert(0, self.snake_position[0] + GRID_DIMENSIONS);
             }
             Direction::Left => {
                 let in_left_column = self.snake_position[0] % GRID_DIMENSIONS == 0;
                 if in_left_column {
                     self.dead = true;
-                    return
+                    return;
                 }
-                
                 self.snake_position.insert(0, self.snake_position[0] - 1);
             }
 
@@ -196,12 +187,11 @@ impl SnakeGame {
                 let in_right_column = (self.snake_position[0] + 1) % GRID_DIMENSIONS == 0;
                 if in_right_column {
                     self.dead = true;
-                    return
+                    return;
                 }
-                
                 self.snake_position.insert(0, self.snake_position[0] + 1);
-            },
-            Direction::None => return
+            }
+            Direction::None => return,
         }
         if self.ate_food() {
             self.create_food();
